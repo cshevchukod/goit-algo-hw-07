f23135024a8526fba10a69cd2707563a7f31d00c
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2026-01-03 16:03:50 +0200

    save

---
+++ task2.py
@@ -0,0 +1,49 @@
+class BinaryTreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.key) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+
+def insert(root, key):
+
+    if root is None:
+        return BinaryTreeNode(key)
+
+    if key < root.key:
+        root.left = insert(root.left, key)
+    else:
+        root.right = insert(root.right, key)
+
+    return root
+
+
+def find_min(root):
+
+    if root is None:
+        return None
+
+    current = root
+    while current.left is not None:
+        current = current.left
+
+    return current.key
+
+
+if __name__ == "__main__":
+    root = None
+    keys = [7, 3, 9, 1, 5, 8, 10, 2]
+
+    for key in keys:
+        root = insert(root, key)
+
+    print(root)
+    print("Min =", find_min(root))  # очікувано 1
